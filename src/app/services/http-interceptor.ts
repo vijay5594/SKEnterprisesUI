469b451dbf067f640a17dd33a181541b92ad429f
salman <mohamedsalmankhan509>
2022-08-11 16:39:52 +0530

    final

--- src/app/services/http-interceptor.ts
+++ src/app/services/http-interceptor.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthCookie } from './auth-cookie-handler';
+
 
 @Injectable()
 export class GlobalHTTPInterceptor implements HttpInterceptor {
-    constructor(private authCookie: AuthCookie) { }
+    constructor() { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         // HttpHeader object immutable - copy values
         const headerSettings: { [name: string]: string | string[]; } = {};
 
-        headerSettings['Authorization'] = this.authCookie.getToken();
+        // headerSettings['Authorization'] = this.authCookie.getToken();
 
         const newHeader = new HttpHeaders(headerSettings);
 
